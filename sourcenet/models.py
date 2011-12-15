29250d42f21206dd0df821a9de84f9552fe5cd88
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2011-12-14 21:43:37 -0500

    2011-12-14 - Jonathan Morgan - trying to fix problem with storing 0 in decimal field.

--- sourcenet/models.py
+++ sourcenet/models.py
@@ -2472,7 +2472,7 @@ class Temp_Section( models.Model ):
         else:
         
             # either no in-house or total is 0.  Set to 0.
-            my_percent_in_house = 0
+            my_percent_in_house = Decimnal( 0 )
             
         #-- END check to make sure values are OK for calculating percent --#
         
@@ -2485,7 +2485,7 @@ class Temp_Section( models.Model ):
         else:
         
             # either no external or total is 0.  Set to 0.
-            my_percent_external = 0
+            my_percent_external = Decimal( 0 )
             
         #-- END check to make sure values are OK for calculating percent --#
        
