a42113976373108688791ccb16b34db4e0b19570
Jonathan Morgan <jonathan.scott.morgan@gmail.com>
2014-08-11 12:42:34 -0400

    2014-08-11 - Jonathan Morgan - added example documents for two NLP APIs.

---
+++ examples/Alchemy_API.py
@@ -0,0 +1,3 @@
+# import AlchemyAPI object
+# installation instructions: http://www.alchemyapi.com/developers/getting-started-guide/using-alchemyapi-with-python/
+from alchemyapi_python.alchemyapi import AlchemyAPI
