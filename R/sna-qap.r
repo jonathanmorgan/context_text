6d1a321344895344b14656bda3fe9ece942fd321
Jonathan Morgan <jonathan.scott.morgan@gmail.com>
2015-04-28 00:45:10 -0400

    refinements.

--- R/sna-qap.r
+++ R/sna-qap.r
@@ -16,4 +16,19 @@ calais_network_ties <- calais_network_data[ , -ncol( calais_network_data )]
 human_network_matrix <- as.matrix( human_network_ties )
 calais_network_matrix <- as.matrix( calais_network_ties )
 
-# package up data for calling qaptest()
+# package up data for calling qaptest() - first make 3-dimensional array to hold
+#    our two matrices - this is known as a "graph set".
+g <- array( dim = c( 2,341,341 ) )
+
+# then, place each matrix in one dimension of the array.
+g[ 1, , ] <- human_network_matrix
+g[ 2, , ] <- calais_network_matrix
+
+# first, just try a graph correlation
+graph_correlation <- gcor( human_network_matrix, calais_network_matrix )
+graph_correlation
+
+# try a qaptest...
+qap_result <- qaptest( g, gcor, g1 = 1, g2 = 2 )
+summary( qap_result )
+plot( qap_result )
