51c2e4032d61292dc8f3286be7dfe2c2703f70a0
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2019-04-22 22:37:32 +0000

    using shared LookupParent from python_utilities.

--- lookups.py
+++ lookups.py
@@ -50,6 +50,9 @@ from context_text.models import Person
 # python_utilities - logging
 from python_utilities.logging.logging_helper import LoggingHelper
 
+# python_utilities - django_ajax_selects_lookup_helper
+from python_utilities.django_utils.django_ajax_selects_lookup_helper import LookupParent
+
 # ajax_select imports
 from ajax_select import register, LookupChannel
 
@@ -119,144 +122,6 @@ def output_debug( message_IN, method_IN = "", indent_with_IN = "", logger_name_I
     #-- END check to see if message. --#
 
 #-- END method output_debug() --#
-
-
-#===============================================================================#
-# Parent Lookup class
-#===============================================================================#
-
-class LookupParent( LookupChannel ):
-
-    def __init__( self, *args, **kwargs ):
-        
-        # call parent's __init__()
-        super( LookupParent, self ).__init__()
-        
-        # initialize variables
-        self.my_class = None
-        
-    #-- END method __init__() --#
-
-    def format_result( self, instance_IN ):
-
-        """ the search results display in the dropdown menu.  may contain html and multiple-lines. will remove any |  """
-
-        # return reference
-        string_OUT = ''
-
-        # try converting to string.  If exception, try calling method
-        #    output_as_string() instead.
-        try:
-
-            # first, try using str()
-            string_OUT = str( instance_IN )
-
-        except:
-
-            # if we get here, exception converting.  Try output_as_string()
-            string_OUT = instance_IN.output_as_string( True )
-
-        return string_OUT
-
-    #-- END method format_result() --#
-
-
-    def format_item( self, instance_IN ):
-
-        """ the display of a currently selected object in the area below the search box. html is OK """
-
-         # return reference
-        string_OUT = ''
-        
-        # try converting to string.  If exception, try calling method
-        #    output_as_string() instead.
-        try:
-            
-            # first, try using str()
-            string_OUT = str( instance_IN )
-            
-        except:
-            
-            # if we get here, exception converting.  Try output_as_string()
-            string_OUT = instance_IN.output_as_string( True )
-
-        return string_OUT
-
-    #-- END method format_item() --#
-
-
-    # new required method - surprise!
-    def format_item_display( self, instance_IN ):
-        
-        '''
-        accepts item, calls format_item(), returns result.
-        '''
-        
-        # return reference
-        string_OUT = ""
-        
-        # call format_item()
-        string_OUT = self.format_item( instance_IN )
-        
-        return string_OUT
-    
-    #-- END method format_item_display() --#
-
-
-    def get_instance_query( self, q, request, class_IN ):
-
-        """
-            return a query set if q passed in is an integer, and that number is
-            the ID of one of the records in our class's database table.  You
-            also have access to request.user if needed.
-        """
-
-        # return reference
-        query_set_OUT = None
-
-        # define variables
-        my_class = None
-        q_int = -1
-        q_instance = ''
-
-        # store class
-        my_class = class_IN
-
-        # is the q a number?
-        try:
-
-            # try casting the query string to int.
-            q_int = int( q )
-
-            # it is an int.  See if there is a match for this ID.
-            q_instance = my_class.objects.get( pk = q_int )
-
-            # got a match?
-            if ( q_instance ):
-
-                # it is a match - return query set with that article in it.
-                query_set_OUT = my_class.objects.filter( Q( pk__in = [ q ] ) )
-
-            else:
-
-                # no match found.  Return None.
-                query_set_OUT = None
-
-            #-- END check to see if we got an instance. --#
-
-        except:
-
-            # not an integer.  set found to false.
-            #found_instance = False
-            query_set_OUT = None
-
-        #-- END attempt to pull in instance by ID --#
-
-        return query_set_OUT
-
-    #-- END method get_instance_query() --#
-
-#-- END class LookupParent --#
 
 
 #===============================================================================#
