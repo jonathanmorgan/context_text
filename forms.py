da177835222acfc7b1302e0eb0a2d0a4399240eb
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2020-10-15 02:33:32 +0000

    finished implementing django-autocomplete-light. Now to remove django-ajax-selects.

--- forms.py
+++ forms.py
@@ -302,11 +302,11 @@ class ArticleCodingForm( forms.ModelForm ):
         #exclude = [ 'article_data', 'original_person', 'match_confidence_level', 'match_status', 'capture_method', 'create_date', 'last_modified', 'source_type', 'subject_type', 'name', 'verbatim_name', 'lookup_name', 'title', 'more_title', 'organization', 'document', 'topics', 'source_contact_type', 'source_capacity', 'localness', 'notes', 'organization_string', 'more_organization' ]
 
     # AJAX lookup for person.
-    person  = make_ajax_field( Article_Subject, 'person', 'coding_person', help_text = "" )
-    #person = forms.ModelChoiceField(
-    #    queryset = Person.objects.all(),
-    #    widget = autocomplete.ModelSelect2( url = 'autocomplete-person' )
-    #)
+    #person  = make_ajax_field( Article_Subject, 'person', 'coding_person', help_text = "" )
+    person = forms.ModelChoiceField(
+        queryset = Person.objects.all(),
+        widget = autocomplete.ModelSelect2( url = 'autocomplete-person' )
+    )
 
 #-- END ModelForm class ArticleCodingForm --#
 
