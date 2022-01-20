a6dac62b578695deb02f6b9e1f58dc2d35f9174b
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2022-01-19 20:44:44 -0500

    default_auto_field

--- apps.py
+++ apps.py
@@ -4,4 +4,11 @@ from django.apps import AppConfig
 
 
 class Context_TextConfig(AppConfig):
+
     name = 'context_text'
+
+    default_auto_field = 'django.db.models.AutoField'
+    # if you have lots of rows:
+    # default_auto_field = 'django.db.models.BigAutoField'
+
+#-- END AppConfig class Context_TextConfig --#
